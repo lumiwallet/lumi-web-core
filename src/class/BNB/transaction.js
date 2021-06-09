484ad176ddc826d092bb8d49fcb988f95cc16d73
vikowl <vd@lumiwallet.com>
2021-06-09 12:22:43 +0300

    Added docs

--- src/class/BNB/transaction.js
+++ src/class/BNB/transaction.js
@@ -11,7 +11,28 @@ import {
 const DEFAULT_CHAIN_ID = 'Binance-Chain-Tigris'
 const DEFAULT_SOURCE = 1
 
+/**
+ * Class BinanceTx.
+ * This class is responsible for calculating the fee list
+ * and generating and signing a Binance transaction
+ * @class
+ */
+
 export default class BinanceTx {
+  /**
+   * Create a BinanceTx
+   * @param {Object} data - Input data for generating a transaction and calculating a fee
+   * @param {number} data.chain_id
+   * @param {string} data.address - Sender's address
+   * @param {number} data.account_number
+   * @param {number} data.sequence
+   * @param {number} data.source
+   * @param {Array} data.fee - Fee list
+   * @param {Array} data.balance - Sender's balance
+   * @param {string} data.privateKey
+   * @param {string} data.publicKey
+   */
+
   constructor (data) {
     data = data || {}
     this.chain_id = data.chain_id || DEFAULT_CHAIN_ID
@@ -28,7 +49,12 @@ export default class BinanceTx {
     this.signatures = []
     this.feeList = []
   }
-  
+
+  /**
+   * Calculating a fee list
+   * @returns {Array} Returns a set of fees
+   */
+
   calcFee () {
     for (let item of this.fee) {
       let fee = {
@@ -36,19 +62,22 @@ export default class BinanceTx {
         fee: convertToBinance(item.fee),
         value: item.fee
       }
-      
+
       this.feeList.push(fee)
     }
     return this.feeList
   }
-  
+
   /**
    * sign transaction with a given private key and msg
-   * @param {string} privateKey private key hex string
-   * @param {SignMsg} concrete msg object
+   * @param {Object} data - Input data for a transaction
+   * @param {string} data.addressTo - Recipient address
+   * @param {number} data.amount - The transaction amount
+   * @param {number} data.fee - The transaction fee
+   * @param {string} data.memo - The transaction memo
    * @return {Transaction}
    **/
-  //todo
+
   make (data) {
     this.memo = data.memo || ''
     this.msg = this.getSignMsg(data)
@@ -61,18 +90,23 @@ export default class BinanceTx {
     this.addSignature(this.publicKey, signature)
     return this
   }
-  
+
+  /**
+   * Getting signed transaction message
+   * @returns {Object}
+   */
+
   getSignMsg (data) {
     const {addressTo, amount, fee} = data
     const feeInJager = convertToJager(fee)
     const amountInJager = convertToJager(amount)
     const fullAmount = bigDecimal.add(feeInJager, amountInJager)
     const change = bigDecimal.subtract(this.balance, fullAmount)
-    
+
     if (change < 0) {
       throw new Error('Insufficient balance')
     }
-    
+
     const inputs = [
       {
         coins: [
@@ -84,7 +118,7 @@ export default class BinanceTx {
         address: this.address
       }
     ]
-    
+
     const outputs = [
       {
         address: addressTo,
@@ -96,14 +130,18 @@ export default class BinanceTx {
         ]
       }
     ]
-    
+
     return {
       inputs,
       outputs
     }
   }
-  
-  // todo
+
+  /**
+   * Getting the transaction message
+   * @returns {Object}
+   */
+
   getMsg (msg) {
     msg.inputs = msg.inputs.map(item => {
       return {
@@ -117,16 +155,17 @@ export default class BinanceTx {
         coins: item.coins
       }
     })
-    
+
     msg.aminoPrefix = AminoPrefix.msg
     return msg
   }
-  
+
   /**
-   * generate the sign bytes for a transaction, given a msg
-   * @param {Object} concrete msg object
+   * Generate the sign bytes for a transaction, given a msg
+   * @param {Object} msg - msg object
    * @return {Buffer}
-   **/
+   */
+
   getSignBytes (msg) {
     const signMsg = {
       account_number: this.account_number.toString(),
@@ -139,12 +178,13 @@ export default class BinanceTx {
     }
     return convertObjectToSignBytes(signMsg)
   }
-  
+
   /**
-   * attaches a signature to the transaction
-   * @param {String} pubKey
+   * Attaches a signature to the transaction
+   * @param {string} pubKey
    * @param {Buffer} signature
-   **/
+   */
+
   addSignature (pubKey, signature) {
     const pubKeyBuf = this._serializePubKey(Buffer.from(this.publicKey, 'hex')) // => Buffer
 
@@ -156,51 +196,58 @@ export default class BinanceTx {
         sequence: this.sequence
       }
     ]
-    
+
     return this
   }
-  
+
   /**
-   * encode signed transaction to hex which is compatible with amino
+   * Encode signed transaction to hex which is compatible with amino
+   * @returns {string}
    */
+
   serialize () {
     if (!this.signatures) {
       throw new Error('need signature')
     }
     const msg = this.getMsg(this.msg)
-  
+
     const stdTx = {
       msg: [msg],
       signatures: this.signatures,
       memo: this.memo,
       source: this.source,
-      data: "",
+      data: '',
       aminoPrefix: AminoPrefix.tx
     }
-    
+
     const bytes = marshalBinary(stdTx)
     this.rawTx = bytes.toString('hex')
     return this.rawTx
   }
-  
-  //todo
+
+  /**
+   * Getting the transaction hash
+   * @returns {string}
+   */
+
   getHash () {
     if (this.rawTx) {
       return crypto.sha256(this.rawTx).toUpperCase()
     }
-    
+
     return ''
   }
-  
+
   /**
-   * serializes a public key in a 33-byte compressed format.
+   * Serializes a public key in a 33-byte compressed format.
    * @param {Buffer} unencodedPubKey
    * @return {Buffer}
    */
+
   _serializePubKey (unencodedPubKey) {
     let pubBz = crypto.encodeBinaryByteArray(unencodedPubKey)
     pubBz = Buffer.concat([Buffer.from(AminoPrefix.PubKeySecp256k1, 'hex'), pubBz])
-    
+
     return pubBz
   }
 }
